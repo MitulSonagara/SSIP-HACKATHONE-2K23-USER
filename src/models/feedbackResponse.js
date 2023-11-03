601156d18e579c2f090ff23c71c82482aca37403
Mitul Sonagara <mitulkumarsongara@gmail.com>
2023-11-03 16:48:55 +0530

    schema changed

--- src/models/feedbackResponse.js
+++ src/models/feedbackResponse.js
@@ -3,6 +3,8 @@ const moment = require('moment-timezone');
 
 const feedbackResponseSchema = new mongoose.Schema({
     username: String,
+    district: String,
+    station: String,
     feedback: [
         {
             questionText: String,
@@ -20,13 +22,13 @@ const feedbackResponseSchema = new mongoose.Schema({
 feedbackResponseSchema.pre('save', function (next) {
     // Set the Indian time zone (Asia/Kolkata)
     const indianTimezone = 'Asia/Kolkata';
-    
+
     // Format the current date and time
     const currentDatetime = moment().tz(indianTimezone).format('DD-MM-YYYY HH:mm:ss');
-    
+
     this.submissionDate = currentDatetime.split(' ')[0];
     this.submissionTime = currentDatetime.split(' ')[1];
-    
+
     next();
 });
 
